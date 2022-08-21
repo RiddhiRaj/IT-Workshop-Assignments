05f226f6119a696090736d98c76823034f4f8693
Riddhi Raj Ghosh <87946275+RiddhiRaj@users.noreply.github.com>
2022-08-21 16:25:49 +0530

    Change grammar

--- Assignment1/P10.java
+++ Assignment1/P10.java
@@ -4,6 +4,6 @@ class P10{
 		if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
 			System.out.println("\n" + year + " is a Leap year.\n");
 		else
-			System.out.println("\n" + year + " is a not Leap year.\n");
+			System.out.println("\n" + year + " is not a Leap year.\n");
 	    }
     }
