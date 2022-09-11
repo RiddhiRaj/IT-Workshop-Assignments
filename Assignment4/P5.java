3a07261b18f27ad92a22dffe572ad96aecca1f02
riddhiraj <87946275+RiddhiRaj@users.noreply.github.com>
2022-09-11 12:55:15 +0530

    .

--- Assignment4/P5.java
+++ Assignment4/P5.java
@@ -11,3 +11,23 @@ class Rectangle extends Shape{
         System.out.println("Inside area");
     }
 }
+
+class Cube extends Rectangle{
+    public void volume(){
+        System.out.println("Inside volume");
+    }
+}
+
+class P5{
+    public static void main(String args[]){
+        Cube c = new Cube();
+        c.display();
+        c.area();
+        c.volume();
+    }
+}
+
+// Output:
+// Inside display
+// Inside area
+// Inside volume
