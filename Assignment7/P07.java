b36c2501ff29f4b3ac794f3a4d13f385d8c31495
riddhiraj <87946275+RiddhiRaj@users.noreply.github.com>
2022-10-28 01:16:43 +0530

    .

--- Assignment7/P07.java
+++ Assignment7/P07.java
@@ -10,22 +10,22 @@ public class P07 extends Thread {
     }
 
     public static void main(String[] args) throws Exception {
-        P07 ob2 = new P07();
-        P07 ob3 = new P07();
-        P07 ob4 = new P07();
-        P07 ob1 = new P07();
+        P07 ob2 = new P07(); // Thread 1
+        P07 ob3 = new P07(); // Thread 2
+        P07 ob4 = new P07(); // Thread 3
+        P07 ob1 = new P07(); // Thread 4
 
-        ob1.setPriority(1);
+        ob1.setPriority(1); // set priority of thread 1
         ob2.setPriority(3);
         ob3.setPriority(5);
         ob4.setPriority(7);
 
-        ob1.start();
+        ob1.start(); // start thread 1
         ob2.start();
         ob3.start();
         ob4.start();
 
-        ob1.join();
+        ob1.join(); // wait for thread 1 to terminate
         ob2.join();
         ob3.join();
         ob4.join();
